5eb58430e11f68b7e005e7a4a4e39d8ecf9158e3
wanderley rabelo <padawandr@gmail.com>
2021-06-10 14:48:45 -0300

    feat: add api get one tour

--- src/app.js
+++ src/app.js
@@ -4,29 +4,49 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-// middleware
+// middlewares
 app.use(express.json())
 
 const toursPath = path.join(__dirname, 'dev-data/data/tours-simple.json')
-const tours = JSON.parse(fs.readFileSync(toursPath)) // json to obj
+const tours = JSON.parse(fs.readFileSync(toursPath))
 
+// get all tours
 app.get('/api/v1/tours', (request, response) => {
-  response.status(200).json({ // obj to json
+  response.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours }
   })
 })
 
+// get one tour
+app.get('/api/v1/tours/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const tour = tours[id]
+
+  if (tour) {
+    response.status(200).json({
+      status: 'success',
+      data: { tour }
+    })
+  } else {
+    response.status(404).json({
+      status: 'fail',
+      message: 'invalid id'
+    })
+  }
+})
+
+// set new tour
 app.post('/api/v1/tours', (request, response) => {
-  const newId = tours.length // tours[tours.length - 1].id + 1
-  const newTour = Object.assign({ id: newId }, request.body) // merge properties
+  const newId = tours.length
+  const newTour = Object.assign({ id: newId }, request.body)
 
   tours.push(newTour)
 
-  fs.writeFile(toursPath, JSON.stringify(tours), () => { // obj to json
-    response.status(201).json({ // obj to json
-      status: 'created',
+  fs.writeFile(toursPath, JSON.stringify(tours), () => {
+    response.status(201).json({
+      status: 'success',
       data: { newTour }
     })
   })
