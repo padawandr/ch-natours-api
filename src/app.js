f31fed137a2f1fd7adea3f72281f8afa40cf9276
wanderley rabelo <padawandr@gmail.com>
2021-06-10 14:04:39 -0300

    feat: add api set new tour

--- src/app.js
+++ src/app.js
@@ -4,15 +4,31 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-const tours = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'dev-data/data/tours-simple.json'))
-)
+// middleware
+app.use(express.json())
+
+const toursPath = path.join(__dirname, 'dev-data/data/tours-simple.json')
+const tours = JSON.parse(fs.readFileSync(toursPath)) // json to obj
 
 app.get('/api/v1/tours', (request, response) => {
-  response.status(200).json({
+  response.status(200).json({ // obj to json
     status: 'success',
     results: tours.length,
     data: { tours }
+  })
+})
+
+app.post('/api/v1/tours', (request, response) => {
+  const newId = tours.length // tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, request.body) // merge properties
+
+  tours.push(newTour)
+
+  fs.writeFile(toursPath, JSON.stringify(tours), () => { // obj to json
+    response.status(201).json({ // obj to json
+      status: 'created',
+      data: { newTour }
+    })
   })
 })
 
