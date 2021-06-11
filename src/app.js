e750753b98fd29ce31a2a8fe1bc397d5df51ea17
wanderley rabelo <padawandr@gmail.com>
2021-06-11 16:01:16 -0300

    feat: add api patch and delete tour placeholders

--- src/app.js
+++ src/app.js
@@ -10,17 +10,29 @@ app.use(express.json())
 const toursPath = path.join(__dirname, 'dev-data/data/tours-simple.json')
 const tours = JSON.parse(fs.readFileSync(toursPath))
 
-// get all tours
-app.get('/api/v1/tours', (request, response) => {
+const getAllTours = (request, response) => {
   response.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours }
   })
-})
+}
 
-// get one tour
-app.get('/api/v1/tours/:id', (request, response) => {
+const setNewTour = (request, response) => {
+  const newId = tours.length
+  const newTour = Object.assign({ id: newId }, request.body)
+
+  tours.push(newTour)
+
+  fs.writeFile(toursPath, JSON.stringify(tours), () => {
+    response.status(201).json({
+      status: 'success',
+      data: { newTour }
+    })
+  })
+}
+
+const getTour = (request, response) => {
   const id = Number(request.params.id)
   const tour = tours[id]
 
@@ -35,22 +47,46 @@ app.get('/api/v1/tours/:id', (request, response) => {
       message: 'invalid id'
     })
   }
-})
+}
 
-// set new tour
-app.post('/api/v1/tours', (request, response) => {
-  const newId = tours.length
-  const newTour = Object.assign({ id: newId }, request.body)
-
-  tours.push(newTour)
-
-  fs.writeFile(toursPath, JSON.stringify(tours), () => {
-    response.status(201).json({
+const updateTour = (request, response) => {
+  if (Number(request.params.id) < tours.length) {
+    response.status(200).json({
       status: 'success',
-      data: { newTour }
+      data: {
+        tour: '# updated tour here... #'
+      }
     })
-  })
-})
+  } else {
+    response.status(404).json({
+      status: 'fail',
+      data: 'invalid id'
+    })
+  }
+}
+
+const deleteTour = (request, response) => {
+  if (Number(request.params.id) < tours.length) {
+    response.status(204).json({
+      status: 'success',
+      data: null
+    })
+  } else {
+    response.status(404).json({
+      status: 'fail',
+      data: 'invalid id'
+    })
+  }
+}
+
+app.route('/api/v1/tours')
+  .get(getAllTours)
+  .post(setNewTour)
+
+app.route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour)
 
 app.listen(3333, (err) => {
   if (err) console.log(`🔴 server error: ${err}`)
