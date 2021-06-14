cafdb8238c81af9cbd589489584b26f78a3257b1
wanderley rabelo <padawandr@gmail.com>
2021-06-14 15:47:03 -0300

    feat: add environment variables

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const userRouter = require('./routes/userRoutes')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('dev'))
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use((request, response, next) => {
   request.time = new Date().toISOString()
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config({ path: path.join(__dirname, '/config.env') })
+
 const app = require('./app')
 
-app.listen(3333, (err) => {
+const port = process.env.PORT || 3333
+app.listen(port, (err) => {
   if (err) console.log(`🔴 server error: ${err}`)
   else console.log('🟢 back-end started...')
 })
